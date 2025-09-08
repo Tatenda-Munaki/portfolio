e7250f2888f3d3ff02f129f5880d4cb52e9045d0
Tatenta <munakite@clarkson.edu>
2025-09-07 22:16:47 -0400

    Fix: add Procfile, requirements, Flask app & index

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
 @app.route("/")
 def home():
+    # must exist at templates/index.html
     return render_template("index.html")
 
+# allows local run and cloud run
 if __name__ == "__main__":
-    # Local run only; Railway will use Gunicorn via Procfile
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
